72f2abf9a762705464797a48cc07c1964e8e5eb3
Ashley Felton <ashley@ropable.com>
2017-12-01 13:59:41 +0800

    Non-working majority_element solution.

--- course1/majority_element.py
+++ course1/majority_element.py
@@ -1,6 +1,39 @@
 # Uses python3
 import sys
 
+"""
+The algorithm begins by splitting the array in half repeatedly and calling itself
+on each half.
+When we get down to single elements, that single element is returned as the majority of
+its (1-element) array. At every other level, it will get return values from its two
+recursive calls.
+The key to this algorithm is the fact that if there is a majority element in the
+combined array, then that element must be the majority element in either the left
+half of the array, or in the right half of the array. There are 4 scenarios.
+a. Both return “no majority.” Then neither half of the array has a majority
+element, and the combined array cannot have a majority element. Therefore,
+the call returns “no majority.”
+b. The right side is a majority, and the left isn’t. The only possible majority for
+this level is with the value that formed a majority on the right half, therefore,
+just compare every element in the combined array and count the number of
+elements that are equal to this value. If it is a majority element then return
+that element, else return “no majority.”
+c. Same as above, but with the left returning a majority, and the right returning
+“no majority.”
+d. Both sub-calls return a majority element. Count the number of elements equal
+to both of the candidates for majority element. If either is a majority element
+in the combined array, then return it. Otherwise, return “no majority.”
+The top level simply returns either a majority element or that no majority element
+exists in the same way.
+"""
+def get_majority_element(a, left, right):
+    if left == right:
+        return -1
+    if left + 1 == right:
+        return a[left]
+    # Your code here
+    return -1
+
 
 def majority(data, tiebreaker=None):
     '''Return the majority element of sequence data, or
@@ -22,24 +55,6 @@ def majority(data, tiebreaker=None):
     major_count = data.count(major)
     if 2 * major_count > n or (2 * major_count == n and major == tiebreaker ):
         return major
-    return -1
-
-
-def get_majority_element(a, left, right):
-    """Doesn't work.
-    """
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
-    i = get_majority_element(a, left, (left + right) // 2)
-    j = get_majority_element(a, (left + right) // 2, right)
-    count_i = 0
-    for el in a:
-        if el == i:
-            count_i += 1
-        if count_i > len(a) // 2:
-            return i
     return -1
 
 
